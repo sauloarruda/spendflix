a569df7facd10bfdbfc5619173e943c3e8073667
Saulo Arruda <sauloarruda@gmail.com>
2025-04-28 09:41:22 +0200

    add turbo config to run workspaces

--- services/auth/src/auth.controller.ts
+++ services/auth/src/auth.controller.ts
@@ -13,7 +13,6 @@ authRouter.post('/signup', async (req, res) => {
     const response = await signup(name, email);
     return res.status(201).json({ message: 'User created', userId: response.User?.Username });
   } catch (error) {
-    console.error(error);
     return res.status(500).json({ message: 'Failed to create user', error });
   }
 });
