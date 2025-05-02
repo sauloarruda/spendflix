dea73a2c5fc10dd9414e980fd8f7d0c96a7734fa
Saulo Arruda <sauloarruda@gmail.com>
2025-05-02 16:02:38 +0200

    remove specific test config

--- services/auth/src/repository/prisma.ts
+++ services/auth/src/repository/prisma.ts
@@ -1,17 +1,16 @@
 import { PrismaClient } from '../../generated/prisma';
-import getConfig from '../../lib/config';
 
 let prisma: PrismaClient;
 
 export default function getPrisma(): PrismaClient {
   if (!prisma) {
-    const config =
-      getConfig().NODE_ENV === 'test'
-        ? {
-            datasourceUrl:
-              'postgresql://postgres:postgres@localhost:5432/spendflix_test?schema=public',
-          }
-        : undefined;
+    const config = {};
+    // getConfig().NODE_ENV === 'test'
+    //   ? {
+    //       datasourceUrl:
+    //         'postgresql://postgres:postgres@localhost:5432/spendflix_test?schema=public',
+    //     }
+    //   : undefined;
 
     prisma = new PrismaClient(config);
   }
