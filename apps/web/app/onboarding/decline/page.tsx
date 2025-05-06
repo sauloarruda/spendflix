8087a2e59fba578c3aadfcb32716dffc8185dc9f
Saulo Arruda <sauloarruda@gmail.com>
2025-05-06 16:05:16 +0200

    feat: add DeclinePage component for onboarding process

---
+++ apps/web/app/onboarding/decline/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function DeclinePage() {
+  return (
+    <>
+      <h2 className="text-2xl font-semibold mb-4 mt-8 text-center">Que pena 😞</h2>
+
+      <p className="text-gray-600 text-center mb-8 max-w-md">
+        Não foi dessa vez, mas desejamos todo sucesso na sua jornada financeira!
+      </p>
+    </>
+  );
+}
