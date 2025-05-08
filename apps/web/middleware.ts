86364058493f56b0ae7e235746d79097887e73f7
Saulo Arruda <sauloarruda@gmail.com>
2025-05-08 11:05:42 +0200

    build: debug middleware env

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,10 +1,13 @@
-// import getLogger from '@/common/logger';
+import getLogger from '@/common/logger';
 import { JwtVerifier } from 'aws-jwt-verify';
 import { validateCognitoJwtFields } from 'aws-jwt-verify/cognito-verifier';
-import { METHODS } from 'http';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
+
+const logger = getLogger().child({ module: 'middleware' });
+
+logger.debug(process.env, 'Environment variables');
 
 const verifier = JwtVerifier.create({
   issuer: `${process.env.COGNITO_ENDPOINT}/${process.env.COGNITO_USER_POOL_ID}`,
@@ -19,9 +22,9 @@ const verifier = JwtVerifier.create({
 async function checkToken(token: string) {
   try {
     const payload = await verifier.verify(token);
-    console.log({ payload }, 'Decoded JWT');
+    logger.debug({ payload }, 'Decoded JWT');
   } catch (error) {
-    console.log({ error }, 'Error verifying token:');
+    logger.error({ error }, 'Error verifying token:');
     throw error;
   }
 }
@@ -29,20 +32,20 @@ async function checkToken(token: string) {
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   if (request.method !== 'POST') return NextResponse.next();
-  console.log('middleware', request.nextUrl.pathname, request.method);
+  logger.debug('middleware', request.nextUrl.pathname, request.method);
   const path = request.nextUrl.pathname;
   if (path === '/onboarding/step1') NextResponse.next();
 
   const cookie = (await cookies()).get('session')?.value;
   if (!cookie) {
-    console.log('No cookie found');
+    logger.warn('No cookie found');
     return Response.json({ success: false, message: 'session cookie not found' }, { status: 401 });
   }
   try {
     const res = await checkToken(cookie);
   } catch (error) {
+    logger.warn({ error }, 'Invalid session');
     return Response.json({ success: false, message: 'invalid session' }, { status: 401 });
   }
-  //   logger.info({ res }, 'Token verified');
   return NextResponse.next();
 }
