2eb28779e6444f5ba7297455fc2eb9c5163c949a
Saulo Arruda <sauloarruda@gmail.com>
2025-05-22 15:34:53 +0200

    fix: try to cache prisma client

--- modules/common/prisma.ts
+++ modules/common/prisma.ts
@@ -3,23 +3,29 @@ import { PrismaPg } from '@prisma/adapter-pg';
 
 import getConfig from './config';
 
-const prismaClientPropertyName = '__prevent-name-collision__prisma';
-type GlobalThisWithPrismaClient = typeof globalThis & {
-  [prismaClientPropertyName]: PrismaClient;
-};
+// const prismaClientPropertyName = '__prevent-name-collision__prisma';
+// type GlobalThisWithPrismaClient = typeof globalThis & {
+//   [prismaClientPropertyName]: PrismaClient;
+// };
+
+let prismaClient: PrismaClient;
 
 function createClient() {
-  const adapter = new PrismaPg({ connectionString: getConfig().DATABASE_URL });
-  return new PrismaClient({ adapter });
+  if (!prismaClient) {
+    const adapter = new PrismaPg({ connectionString: getConfig().DATABASE_URL });
+    prismaClient = new PrismaClient({ adapter });
+  }
+  return prismaClient;
 }
 
 export default function getPrisma(): PrismaClient {
-  if (process.env.NODE_ENV === 'production') {
-    return createClient();
-  }
-  const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
-  if (!newGlobalThis[prismaClientPropertyName]) {
-    newGlobalThis[prismaClientPropertyName] = createClient();
-  }
-  return newGlobalThis[prismaClientPropertyName];
+  return createClient();
+  // if (process.env.NODE_ENV === 'production') {
+  //   return createClient();
+  // }
+  // const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
+  // if (!newGlobalThis[prismaClientPropertyName]) {
+  //   newGlobalThis[prismaClientPropertyName] = createClient();
+  // }
+  // return newGlobalThis[prismaClientPropertyName];
 }
