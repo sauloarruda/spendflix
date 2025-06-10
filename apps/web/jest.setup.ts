4b27cb627cd1ce9db85aee28b2bac95f127e14de
Saulo Arruda <sauloarruda@gmail.com>
2025-06-10 07:38:02 -0300

    test: downgrade chalk

--- apps/web/jest.setup.ts
+++ apps/web/jest.setup.ts
@@ -18,18 +18,3 @@ console.error = (...args) => {
   }
   originalError(...args);
 };
-
-// Mock chalk module
-jest.mock('chalk', () => ({
-  default: {
-    magenta: (text: string) => text,
-    red: (text: string) => text,
-    green: (text: string) => text,
-    yellow: (text: string) => text,
-    blue: (text: string) => text,
-    cyan: (text: string) => text,
-    white: (text: string) => text,
-    gray: (text: string) => text,
-    black: (text: string) => text,
-  },
-}));
