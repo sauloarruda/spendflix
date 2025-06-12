a1f31c941e4727c6a9ad755a9152026b2fba2d79
Saulo Arruda <sauloarruda@gmail.com>
2025-06-12 06:28:41 -0300

    style: remove unuseful values in logger

--- modules/common/logger.ts
+++ modules/common/logger.ts
@@ -80,7 +80,7 @@ class ConsoleLogger {
     const levelStr = this.bindings.module
       ? [chalk.magenta(`[${this.bindings.module}]`), level].join(' ')
       : level;
-    const bindingsObj = omit(this.bindings, 'module');
+    const bindingsObj = omit(this.bindings, ['module', 'level', 'isOffline']);
     const otherArgs = Object.keys(bindingsObj) ? { ...bindingsObj, ...args } : { ...args };
     if (msg) console.log(levelStr, msg, obj, otherArgs);
     else console.log(levelStr, obj, bindingsObj, otherArgs);
